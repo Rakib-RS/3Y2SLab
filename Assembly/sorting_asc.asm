f1504a1ba113a10d01ba763b3e1fa7f4fe9d64a3
Rakib-RS <rakib.rscse@gmail.com>
2020-02-29 17:54:14 +0600

    assembly code

---
+++ Assembly/sorting_asc.asm
@@ -0,0 +1,77 @@
+.MODEL SMALL
+.STACK 100H    
+
+.DATA
+	str DB 80 DUP ?
+	MSG DB "ENTER A LETTER STRING WITH SAME CASE: $"
+	RES DB "Ascending Order: $" 
+.CODE   
+    MAIN PROC
+    MOV AX, @DATA
+	MOV DS, AX
+	
+	MOV AH,9
+	LEA DX, MSG
+	INT 21H
+	
+	MOV AH,1
+	MOV SI,0
+TOP:
+	INT 21H
+	CMP AL,0DH
+	JZ EXIT
+	
+	MOV str[SI], AL
+	
+	INC SI
+    JMP TOP
+	
+	EXIT:
+	;DEC SI
+	MOV CX, SI
+	;sorting
+	
+LOP1:
+	DEC SI
+	MOV DI,SI
+LOP2:
+    DEC DI
+	MOV BL, str[SI]
+	CMP BL, str[DI]
+	JG SKIP
+	XCHG BL, str[DI]
+	MOV str[SI],BL
+	
+SKIP:
+    CMP DI,0
+	JNE LOP2
+S2:
+    CMP SI,1
+	JG LOP1
+		 
+;print a new line
+
+	MOV AH, 2
+	MOV DL, 0AH
+	INT 21H
+	MOV DL, 0DH
+	INT 21H
+	
+	MOV AH,9
+	LEA DX, RES
+	INT 21H
+;Output
+	MOV DI, 0
+	MOV AH,2
+NEXT:
+	MOV DL, str[DI]
+	INT 21H
+	INC DI
+	CMP DI,CX
+	JB NEXT
+	
+	MOV AH,4CH
+	MAIN ENDP
+	END MAIN
+    
+    
---
+++ Assembly/vowel_cons_count.asm
@@ -0,0 +1,167 @@
+.MODEL SMALL
+.STACK 100H    
+
+.DATA
+	str DB 80 DUP ?
+	MSG DB "ENTER A STRING:$"
+	vowel_ DB "Number of Vowel: $"
+	cons_ DB "Number of Consonants: $"
+	space_ DB "Number of Space: $"
+	number_ DB "Number of Digits: $"
+	bc DB '0'
+	cc DB '0'
+	space DB '0'
+	num DB '0'    
+
+.CODE   
+    MAIN PROC
+    MOV AX, @DATA
+	MOV DS, AX
+	
+	MOV AH,9
+	LEA DX, MSG
+	INT 21H
+	
+	MOV BL,48           
+	MOV CL,48
+	MOV SI,0
+	MOV AH,1 
+	
+NEXT:
+	INT 21H
+	CMP AL,0DH
+	JZ EXIT
+	
+	MOV str[SI], AL
+	
+	;comparing
+L1: CMP str[SI],'A'
+    JL L
+    CMP str[SI],'Z'
+    JG L2
+    JMP L3
+L2: CMP str[SI],'a'
+    JL L
+    CMP str[SI],'z'
+    JG L
+    JMP L3
+
+	;COMPARISION FOR VOWEL
+L3:
+	CMP str[SI],'A'  
+	JZ vowel
+	CMP str[SI],'a'
+	JZ vowel
+	CMP str[SI],'E'
+	JZ vowel
+	CMP str[SI],'e'
+	JZ vowel
+	CMP str[SI],'I'
+	JZ vowel
+	CMP str[SI],'i'
+	JZ vowel
+	CMP str[SI],'O'
+	JZ vowel       
+	CMP str[SI],'o'
+	JZ vowel
+	CMP str[SI],'U'
+	JZ vowel
+	CMP str[SI],'u'
+	JZ vowel       
+	JMP cons
+vowel:
+	ADD bc,1
+	jmp L
+cons:
+	ADD cc,1
+L:  
+    CMP str[SI],' '
+    JNE number
+    ADD space, 1
+    JMP ignore
+number:
+    CMP str[SI],'0'
+    JL ignore
+    CMP str[SI],'9'
+    JG ignore
+    ADD num,1
+ignore:
+    INC SI
+    JMP NEXT
+    
+EXIT:
+	DEC SI
+	;print a new line
+	MOV AH, 2
+	MOV DL, 0AH
+	INT 21H
+	MOV DL, 0DH
+	INT 21H
+	
+	;output vowel
+	MOV AH,9
+	LEA DX, vowel_
+	INT 21H 
+	
+	MOV AH,2
+	MOV DL,bc
+	INT 21H  
+	
+	;print a new line
+	MOV AH, 2
+	MOV DL, 0AH
+	INT 21H
+	MOV DL, 0DH
+	INT 21H   
+	
+	;output consonants
+	MOV AH,9
+	LEA DX, cons_
+	INT 21H
+	
+	MOV AH, 2
+	MOV DL,cc
+	INT 21H
+	
+	;print a new line
+	MOV AH, 2
+	MOV DL, 0AH
+	INT 21H
+	MOV DL, 0DH
+	INT 21H
+	
+	;output space
+	MOV AH,9
+	LEA DX, space_
+	INT 21H
+	
+	MOV AH, 2
+	MOV DL,space
+	INT 21H
+	
+	;print a new line
+	MOV AH, 2
+	MOV DL, 0AH
+	INT 21H
+	MOV DL, 0DH
+	INT 21H
+	
+	;output numbers
+	MOV AH,9
+	LEA DX, number_
+	INT 21H
+	
+	MOV AH, 2
+	MOV DL,num
+	INT 21H
+	
+	MOV AH, 4CH
+	INT 21H
+
+	MOV AH,4CH
+MAIN ENDP
+END MAIN
+	
+	
+	
+	
